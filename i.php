515fa8284eedc10275691bfa3af1c2f8a5df70e3
Matthieu ONFRAY <18144846+matthieuonfray@users.noreply.github.com>
2020-03-05 10:52:04 +0100

    Update i.php

--- i.php
+++ i.php
@@ -1,18 +1,19 @@
 <?php
-//fichier du dernier id lu
-define('FILE', 'tweet_id.txt');
 //les ID tweeter
 define('CONSUMER_KEY', '');
 define('CONSUMER_SECRET', '');
 define('ACCESS_TOKEN', '');
 define('ACCESS_TOKEN_SECRET', '');
 //nombre de follower à ramener d'un post
-define('AMIS_NB_FOLLOWER', 1);
+define('AMIS_NB_FOLLOWER', 0);
 //nombe de tweet à monitorer à chaque requête
 define('NB_TWEET_RAMENER', 1);
 define('PROD', true);
 define('USER_t', 'XXX123');
-define('TWEETOS', '@XYZ');
+define('TWEETOS', '@XYZ,@MgXeYZitJ,@FeaFFFFFoFFX');
+define('MAIL_WEBMASTER', 'xx.yy@pm.me');
+define('FILE_log', 'log_bot.txt');
+$mentions_r = array('tag ', 'comment', 'partage', 'identifie', 'mention', 'répond');
 //la recherche 
 define('SRCH_t', 'rt AND (concours OR gagner OR participer OR remporter)');
 if (! PROD)
